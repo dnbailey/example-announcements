17681673c26d1e47b51ec93a7d2782cfd3916197
Nathan Bailey <dev@dnbailey.org>
2017-11-09 18:48:47 -0500

    Create post route

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', require('./routes/index.js'))
 
-app.listen('3000', () => {
-  console.log('Listening on port 3000')
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body.title)
+  res.render('index')
+})
+
 module.exports = router
