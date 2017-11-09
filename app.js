c06292649f567abf768208a49a75ed3a8c4c64c2
Nathan Bailey <dev@dnbailey.org>
2017-11-09 18:27:33 -0500

    Create routes and views

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use('/', require('./routes/index.js'))
+
 app.listen('3000', () => {
   console.log('Listening on port 3000')
 })
